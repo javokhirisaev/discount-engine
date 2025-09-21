ee2b1151d76dbaadbec2875331f07a17dc04144e
Javokhir Isaev <javokhirisaev@usf.edu>
2025-09-21 16:02:15 -0400

    I created an array of products

---
+++ cc_4.js
@@ -0,0 +1,10 @@
+// Step 2
+let products = [
+  { name: "Smartphone", category: "electronics", price: 800, inventory: 5 },
+  { name: "Jeans", category: "apparel", price: 40, inventory: 12 },
+  { name: "Milk", category: "groceries", price: 3, inventory: 15 },
+  { name: "Paper Towels", category: "household", price: 6, inventory: 10 },
+  { name: "Novel", category: "other", price: 18, inventory: 7 }
+];
+
+//
